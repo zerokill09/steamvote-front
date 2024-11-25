5eac9c45af9c028b96c99b03b6b4eb050839ade4
KimYoungGil <yg.k@interiorteacher.com>
2024-11-25 18:33:02 +0900

    test cors

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,20 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['shared.akamai.steamstatic.com'], // 허용할 도메인 추가
   },
+  async headers() {
+    return [
+      {
+        // 모든 API 경로에 대해 CORS 헤더 추가
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
